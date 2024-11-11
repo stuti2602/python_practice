d876a9ca597c1afe71846908b324551eaa46b94e
stuti2602 <stutishah2602@gmail.com>
2024-11-11 23:42:21 +0530

    day 2 problem

---
+++ problem_2.py
@@ -0,0 +1,25 @@
+"""
+Ask the user to input a list of numbers, seperated by spaces.
+Convert the input into a list of intergers.
+Calculate the sum of all numbers in the list.
+Print the result in the format: "The sum of numbers is [sum]."
+"""
+
+def sum_of_list(convert_into_list):
+
+    sum = 0
+
+    for element in convert_into_list:
+        sum += element
+    return sum
+
+
+def main():
+    user_input = input("Enter a list of numbers seperated by spaces: ")
+    convert_into_list = list(map(int,user_input.split()))
+    result = sum_of_list(convert_into_list)
+    print(f'The sum of numbers is {result}')
+
+if __name__ == "__main__":
+    main()
+
