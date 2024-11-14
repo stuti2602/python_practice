b8fd3d852bd7424c339dbb16350785f4c29abee5
stuti2602 <stutishah2602@gmail.com>
2024-11-15 00:47:02 +0530

    daily problem solve question and solution

---
+++ problem_3.py
@@ -0,0 +1,15 @@
+"""
+write a pyhton program that asks user to input a number and prints the multiplication table for that number from 1 to 10.
+"""
+
+def multiplication_table(input_number):
+    for i in range(1,11):
+        multi = input_number * i
+        print(f"{input_number} * {i} = {multi}")
+
+def main():
+    input_number = int(input("Enter a number: "))
+    multiplication_table(input_number)
+
+if __name__ == "__main__":
+    main()
